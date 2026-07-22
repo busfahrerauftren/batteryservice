2a2ec24cbc63af1c9597a2befbeeed60bdabe0eb
busfahrerauftren <146971481+busfahrerauftren@users.noreply.github.com>
2026-07-22 15:11:05 +0200

    Add files via upload

---
+++ src/ExampleCalculationService/example_battery_service_dataclasses.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class MaxChargeAndDischargeOutput:
+    max_charge_active_power : float | None = None
+    max_discharge_active_power : float | None = None
+
+
